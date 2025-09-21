a1fab0ada2552c32961e91845fb22a154e03a3de
Bala9840 <barathbalaji9840@gmail.com>
2025-09-21 12:18:33 +0530

    Newsfeed Removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import Watchlist from './components/Watchlist';
-import NewsFeed from "./components/NewsFeed";
+// import NewsFeed from "./components/NewsFeed";
 
 import './App.css';
 
@@ -15,8 +15,8 @@ function App() {
         return <Dashboard />;
       case 'watchlist':
         return <Watchlist />;
-      case 'news':
-        return <NewsFeed />;
+      // case 'news':
+      //   return <NewsFeed />;
       default:
         return <Dashboard />;
     }
